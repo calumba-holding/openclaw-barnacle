bc374ebbbb248b530ff0b193588e86fc1305985e
Shadow <hi@shadowing.dev>
2026-01-25 01:38:29 -0600

    Update guide and help command text

--- src/commands/guide.ts
+++ src/commands/guide.ts
@@ -1,4 +1,4 @@
-import { type CommandInteraction, TextDisplay } from "@buape/carbon"
+import { ApplicationCommandOptionType, type CommandInteraction } from "@buape/carbon"
 import BaseCommand from "./base.js"
 
 const guideLink = "https://discord.com/channels/1456350064065904867/@home"
@@ -6,12 +6,21 @@ const guideLink = "https://discord.com/channels/1456350064065904867/@home"
 export default class GuideCommand extends BaseCommand {
 	name = "guide"
 	description = "Share the server guide"
+	options = [
+		{
+			name: "user",
+			description: "User to mention",
+			type: ApplicationCommandOptionType.User
+		}
+	]
 
 	async run(interaction: CommandInteraction) {
+		const user = interaction.options.getUser("user")
+		const prefix = user ? `<@${user.id}>\n` : ""
+		const message = `${prefix}## Check the Server Guide here\n${guideLink}`
+
 		await interaction.reply({
-			components: [
-				new TextDisplay(`➡️ [SERVER GUIDE](${guideLink})`)
-			]
+			content: message
 		})
 	}
 }
--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,4 +1,4 @@
-import { type CommandInteraction, TextDisplay } from "@buape/carbon"
+import { ApplicationCommandOptionType, type CommandInteraction } from "@buape/carbon"
 import BaseCommand from "./base.js"
 
 const stuckLink = "https://docs.clawd.bot/help/faq#im-stuck-whats-the-fastest-way-to-get-unstuck"
@@ -6,14 +6,21 @@ const stuckLink = "https://docs.clawd.bot/help/faq#im-stuck-whats-the-fastest-wa
 export default class HelpCommand extends BaseCommand {
 	name = "help"
 	description = "Share help instructions"
+	options = [
+		{
+			name: "user",
+			description: "User to mention",
+			type: ApplicationCommandOptionType.User
+		}
+	]
 
 	async run(interaction: CommandInteraction) {
+		const user = interaction.options.getUser("user")
+		const prefix = user ? `<@${user.id}>\n` : ""
+		const message = `${prefix}Use <#1459642797895319552> for help. The fastest way to get your problem solved is to follow the instructions here: ${stuckLink}`
+
 		await interaction.reply({
-			components: [
-				new TextDisplay(
-					`Use #help for help, but also, The fastest way to get your problem solved is to follow the instructions here: ${stuckLink}`
-				)
-			]
+			content: message
 		})
 	}
 }
